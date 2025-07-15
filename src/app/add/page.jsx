f86e4f0eb9d99330e58ff078d569372eec3efbec
jinseomun <jshin27@pusan.ac.kr>
2025-07-15 15:52:40 +0900

    마감일 등록하기 퍼블리싱

---
+++ src/app/add/page.jsx
@@ -0,0 +1,19 @@
+import SideBarPage from '@/app/main/side-bar';
+import Header from '@/app/add/header'
+import Form from '@/app/add/form'
+
+
+
+export default function AddPage() {
+    
+    return(
+        <div className="h-screen w-full bg-[#F5F7FA] flex overflow-hidden">
+            <SideBarPage/>
+            <div className="h-screen w-full flex flex-col mx-[60px]">
+                <Header submitLabel="등록하기"/>
+                <Form/>
+            </div>  
+            
+        </div>
+    )
+}
