20599b6c40316c1cc38c80821778435f24b2bb1f
qlqlrh <bluebird.happier@gmail.com>
2025-07-31 16:32:47 +0900

    feat: 초대코드 사용 내역 모달 제작, 페이지 연결

--- src/app/invitation-code/page.jsx
+++ src/app/invitation-code/page.jsx
@@ -3,7 +3,7 @@ import Modal2 from './modal2';
 export default function Page() {
   return (
     <main className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <Modal2 />
+      <Modal1 />
     </main>
   );
 }
