d1c07667bdd3bf3df791375caa9b763b72c9356e
narim <ehdrkd648@naver.com>
2025-07-31 19:49:34 +0900

    fix: 경로 수정

--- src/app/client/mypage/modals/admissionModal.jsx
+++ src/app/client/mypage/modals/admissionModal.jsx
@@ -4,7 +4,7 @@ import { useRef, useState } from 'react';
 import ModalLayout from './modalLayout';
 import { MedalIcon } from "@/components/icons/affiliationIcons";
 import { requestFreshmanAffiliation } from '@/app/api-service/studentAffiliationApi';
-import { Input } from '@/components/search';
+import { Input } from '@/components/input';
 
 export function AdmissionModal({ onClose, onBack }) {
     const [file, setFile] = useState(null);
